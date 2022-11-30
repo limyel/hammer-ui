7b3854c7dade3c63aac3b9c31951f1556bb46d84
limyel <limyel@outlook.com>
2022-11-30 22:26:38 +0800

    all

---
+++ src/views/account/Login.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="login">
+    <div class="form-wrap">
+
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Login"
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
